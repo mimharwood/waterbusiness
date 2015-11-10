188bf9813953bad9a663bf8751647063daa3a12d
Mim Harwood <mim.harwood@gmail.com>
2015-11-09 21:47:42 -0500

    store2.html and masonJarStyle

--- js/StoreScripts.js
+++ js/StoreScripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $(".dropdown-toggle").dropdown("toggle");
+
+  	$('.boat').each(function(i){ $(this).css('animation-delay', -i+'s')});
+  $('[data-float-wrong]').css( {
+    'float':'none',
+    'position' : 'relative',
+    'top' : '150px',
+    'transform' : 'rotate(40deg)'
+  });
 });
